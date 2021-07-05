619eac652ca6362f9319ff22be8629f1121f7182
Pooja Singh <84841094+poojasingh1mg@users.noreply.github.com>
2021-07-05 22:33:45 +0530

    Test

---
+++ app/src/main/java/com/poojasingh/tutorialkotlin/rxExample/RxJavaExamplesActivity.kt
@@ -0,0 +1,11 @@
+package com.poojasingh.tutorialkotlin.rxExample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RxJavaExamplesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rx_java_examples)
+    }
+}
