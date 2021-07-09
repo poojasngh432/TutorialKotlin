06c6f42bff30db86e9147e8d0f839b6b3097e86f
Pooja Singh <84841094+poojasingh1mg@users.noreply.github.com>
2021-07-09 13:51:58 +0530

    new-branch commit

---
+++ app/src/main/java/com/poojasingh/tutorialkotlin/TestingActivity.kt
@@ -0,0 +1,11 @@
+package com.poojasingh.tutorialkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_testing)
+    }
+}
